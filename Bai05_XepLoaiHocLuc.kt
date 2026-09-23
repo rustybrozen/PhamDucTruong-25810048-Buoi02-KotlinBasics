1165b9813f379d83fa3ad3ac0d9e5434a4985576
Russel <zachudson12@gmail.com>
2026-09-24 00:13:38 +0700

    Buoi02 Bai05 xep loai hoc luc

---
+++ Bai05_XepLoaiHocLuc.kt
@@ -0,0 +1,17 @@
+﻿// Phạm Đức Trường - 25810048
+fun main(){
+    val tb : Double = 7.5
+    
+    when (tb){
+        in 9.0..10.0 -> println("Xuat sac")
+        in 8.0..8.9 -> println("Gioi")
+        in 7.0..7.9 -> println("Kha")
+        in 5.0..6.9 -> println("trung binh")
+        in 0.0..4.9 -> println("yeu")
+        else -> println("Diem trung binh khong hop le")
+    }
+   
+
+ 
+}
+
