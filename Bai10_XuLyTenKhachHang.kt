16291becfabb75e1555c29babd4ccc96a83e83cc
Russel <zachudson12@gmail.com>
2026-09-24 00:13:39 +0700

    Buoi02 Bai10 xu ly ten khach hang co the rong

---
+++ Bai10_XuLyTenKhachHang.kt
@@ -0,0 +1,19 @@
+﻿// Phạm Đức Trường - 25810048
+fun main(){
+    fun hamChuoi(ten String?){
+        val tenSafeCall : Int? = ten?.length
+        val tenElvis String : ten?: "Khach vang lai"
+        val tenHaiThang Int : ten!!.length
+        
+        //doi voi toan tu cham thang, voi 1 doan code null thi code
+        //se loi ngay lap tuc, o day chi dung khi chac chan bien nay
+        //khong null, vi du nhu thong tin nguoi dung: username, password
+        
+        println("Do dai la: $tenSafeCall")
+        println("ten khach: $tenElvis")
+        print("Do dai lan 2: $tenHaiThang")
+    }
+    
+    hamChuoi()
+}
+
