9c1f9da469fc13a2ff641d51ee6c1e100117f8ea
Russel <zachudson12@gmail.com>
2026-09-24 10:16:29 +0700

    Buoi02 Bai08 quan ly danh sach sach

--- Bai08_DanhSachSach.kt
+++ Bai08_DanhSachSach.kt
@@ -1,2 +1,11 @@
 ﻿// Phạm Đức Trường - 25810048
 
+fun main(){
+    val sach = mutableListOf("Anh", "Otp", "Happi", "Sad", "2000")
+    println("Sach ban dau: $sach")
+    sach.add("sach thu 6")
+    sach.add("sach thu 7")
+    sach.remove("Otp")
+    sach.sort()
+    println("Sach da thay doi: $sach")
+}
