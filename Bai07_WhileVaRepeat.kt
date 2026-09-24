daf5e6c19f81497bdd3c64cd0437626e87040a9a
Russel <zachudson12@gmail.com>
2026-09-24 10:16:29 +0700

    Buoi02 Bai07 dem nguoc va lap co dinh

--- Bai07_WhileVaRepeat.kt
+++ Bai07_WhileVaRepeat.kt
@@ -1,2 +1,15 @@
 ﻿// Phạm Đức Trường - 25810048
 
+fun main(){
+    var i : Int = 10
+    while (i > 0){
+        println("Dem nguoc: $i")
+        i--
+    }
+    repeat(5){
+        println("In 5 lan lien tiep")
+    }
+ 
+ //dung while khi biet dieu kien co the dat duoc
+ //dung repeat lap lai cho 1 so co dinh, don gian
+}
