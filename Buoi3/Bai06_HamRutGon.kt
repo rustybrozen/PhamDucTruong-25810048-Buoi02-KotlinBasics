8b85807f1df476269a9f863a3ffa0f33312c452a
Russel <zachudson12@gmail.com>
2026-09-26 16:54:08 +0700

    Buoi03 Bai06 ham rut gon

---
+++ Buoi3/Bai06_HamRutGon.kt
@@ -0,0 +1,35 @@
+// Phạm Đức Trường - 25810048
+
+fun binhPhuong(n: Int): Int {
+    return n * n
+}
+fun rutGonBinhPhuong(n: Int): Int = n * n
+
+
+
+fun chuViHinhVuong(canh: Double): Double {
+    return canh * 4
+}
+fun rutGonChuViHinhVuong(canh: Double): Double = canh * 4
+
+
+
+fun laSoChan(n: Int): Boolean {
+    return n % 2 == 0
+}
+fun rutGonLaSoChan(n: Int): Boolean = n % 2 == 0
+
+
+
+
+    println(binhPhuong(5))          
+    println(rutGonBinhPhuong(5))    
+
+  
+    println(chuViHinhVuong(6.0))       
+    println(rutGonChuViHinhVuong(6.0)) 
+
+
+    println(laSoChan(8))         
+    println(rutGonLaSoChan(8))   
+
