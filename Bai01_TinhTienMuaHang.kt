b22b17edcecdf78942029d01ff2966451bede7bf
Russel <zachudson12@gmail.com>
2026-09-26 17:00:17 +0700

    Don sach file Buoi 2 o root va chuyen vao folder Buoi2

--- Bai01_TinhTienMuaHang.kt
+++
@@ -1,9 +0,0 @@
-﻿// Phạm Đức Trường - 25810048
-fun main(){
-    val sl : Int = 10
-    val donGia: Double = 10000.0
-    val slOne : Double = sl.toDouble()
-    println("Tinh tien banh mi x $sl: ${slOne * donGia} vnd")
-    
-}
-
