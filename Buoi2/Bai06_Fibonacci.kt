0bb6445340400b6412bedd9a2e7322095e10bd6a
Russel <zachudson12@gmail.com>
2026-09-26 16:52:13 +0700

    Gom cac bai Buoi 2 vao thu muc Buoi2

---
+++ Buoi2/Bai06_Fibonacci.kt
@@ -0,0 +1,21 @@
+﻿// Phạm Đức Trường - 25810048
+fun main(){
+    var A : Int = 0
+    var B : Int = 1
+    var C : Int
+    
+    println("Vi tri 0: $A")
+    println("Vi tri 1: $B")
+    
+    for (i in 2 until 100){
+       
+        C = A + B
+        if (C >= 100) {break}
+       println("Vi tri $i: $C")
+        A = B
+        B = C
+        
+    }
+
+}
+
